86ac989c01b3ccc1f80f0c3daccc2ea216819153
Gustavo Benicio <gmb.gustavo@gmail.com>
2023-07-11 15:13:53 -0300

    Reformulacao

--- main.py
+++ main.py
@@ -1,26 +1,55 @@
+import time
 import pikepdf
+import itertools
 
-passwords_filename = "passwords.txt"
+arq_pdf = "/home/gustavo/Downloads/teste.pdf"
+maiusculos = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+minusculos = 'abcdefghijklmnopqrstuvwxyz'
+digitos = '0123456789'
+caracteres = minusculos + maiusculos + digitos
+combinacoes = []
+tamanho = 5
 
-locked_pdf_file = "my_locked.pdf"
 
-# load passwords file
-with open(passwords_filename) as file:
-    passwords_list = file.readlines()
-    total_passwords = len(passwords_list)
+def lista_senha(arq):
+    # Carregar lista de senhas
+    with open(arq) as file:
+        passwords_list = file.readlines()
+        total_passwords = len(passwords_list)
 
-    for index, password in enumerate(passwords_list):
 
-        # try if password is correct
+def gerar(size):
+    for comb in itertools.product(caracteres, repeat=size):
+        senha = ''.join(comb)
+        yield senha
+
+
+def main():
+    total_passwords = len(caracteres) ** tamanho
+    inicio = time.time()
+    for index, password in enumerate(gerar(tamanho)):
         try:
-            with pikepdf.open(locked_pdf_file, password=password.strip()) as pdf_file:
-                print("\n++++++++++++++++++++++SUCCESS+++++++++++++++")
-                print("Success---------- File is Unlocked and the password is: ", password)
+            with pikepdf.open(arq_pdf, password=password.strip()) as pdf_file:
+                print("\n\n-----ENCONTRADO-----")
+                print("File is Unlocked and the password is: ", password)
                 break
-        # if password fail
-        except:
-            print("\n=====================")
-            print(f"Trying Password {password} --- Fail!!!!")
+        # Se o arquivo não for encontrado
+        except FileNotFoundError:
+            print(f'\nArquivo {arq_pdf} não encontradao.')
+            quit(2)
+        # Se a senha falhar
+        except pikepdf.PasswordError:
             scanning = (index / total_passwords) * 100
-            print("Scanning passwords complete:", round(scanning, 2))
+            percentil = "{:.3f}".format(round(scanning, 3))
+            tempo_decorrido = time.time() - inicio
+            senhas_restantes = total_passwords - index - 1
+            tentativas_por_segundo = index / tempo_decorrido if tempo_decorrido > 0 else 0
+            tempo_estimado = senhas_restantes / tentativas_por_segundo if tentativas_por_segundo > 0 else 0
+            print(f'\rProgresso total: {percentil} '
+                  f'--> Senha atual: {password} '
+                  f'--> Tempo estimado = {tempo_estimado / 3600}', end='')
             continue
+
+
+if __name__ == '__main__':
+    main()

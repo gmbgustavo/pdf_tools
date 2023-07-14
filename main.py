2814960d888537edfcc9ee808086cca6c18676e1
Gustavo Benicio <gmb.gustavo@gmail.com>
2023-07-14 12:38:00 -0300

    Reformulacao

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pikepdf
 import itertools
 import concurrent.futures
 
-arq_pdf = "/home/gustavo/Downloads/digitos.PDF"
+arq_pdf = '/home/gustavo/Downloads/digitos.PDF'
 maiusculos = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 minusculos = 'abcdefghijklmnopqrstuvwxyz'
 digitos = '0123456789'
@@ -23,7 +23,6 @@ def lista_senha(arq):
 def terminate(cod):
     quit(cod)
 
-
 def gerar(size):
     # Gerar todas as combinações possĩveis por força bruta
     for comb in itertools.product(caracteres, repeat=size):
@@ -42,6 +41,18 @@ def quebrar_senha(password):
         terminate(2)
 
 
+def print_stats(index, password, inicio):
+    scanning = (index / len(caracteres) ** tamanho) * 100
+    percentil = "{:.3f}".format(round(scanning, 3))
+    tempo_decorrido = time.time() - inicio
+    senhas_restantes = len(caracteres) ** tamanho - index - 1
+    tentativas_por_segundo = index / tempo_decorrido if tempo_decorrido > 0 else 0
+    tempo_estimado = senhas_restantes / tentativas_por_segundo if tentativas_por_segundo > 0 else 0
+    print(f'\rProgresso total: {percentil} '
+          f'--> Senha atual: {password[1]} '
+          f'--> Tempo estimado: {tempo_estimado / 60:.2f} minutos.', end='')
+
+
 def main():
     total_passwords = len(caracteres) ** tamanho
     inicio = time.time()
@@ -56,15 +67,7 @@ def main():
                 print("A senha encontrada é: ", password[0])
                 terminate(0)
             else:
-                scanning = (index / len(caracteres) ** tamanho) * 100
-                percentil = "{:.3f}".format(round(scanning, 3))
-                tempo_decorrido = time.time() - inicio
-                senhas_restantes = len(caracteres) ** tamanho - index - 1
-                tentativas_por_segundo = index / tempo_decorrido if tempo_decorrido > 0 else 0
-                tempo_estimado = senhas_restantes / tentativas_por_segundo if tentativas_por_segundo > 0 else 0
-                print(f'\rProgresso total: {percentil} '
-                      f'--> Senha atual: {password[1]} '
-                      f'--> Tempo estimado: {tempo_estimado / 60:.2f} minutos.', end='')
+                print_stats(index, password, inicio)
 
     tempo_decorrido = time.time() - inicio
     print(f"\nTempo decorrido: {tempo_decorrido:.2f} segundos")
@@ -72,4 +75,5 @@ def main():
 
 if __name__ == '__main__':
     main()
+    print('Não encontrado no range especificado.')
     quit(5)
